d89452abb6d22daf2d09e7cbb64f53d72d78fea7
Michael Uloth <hello@michaeluloth.com>
2018-02-24 21:01:31 -0500

    Take two, putting Object.assign first in the polyfill request

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -26,18 +26,15 @@ exports.onClientEntry = () => {
   // Test required JS features:
   const browserSupportsAllJsFeatures = () => {
     // NOTE: Gatsby includes a Promise polyfill already (don't duplicate it here)
-    const intersectionObserver =
-      window.IntersectionObserver && window.IntersectionObserverEntry && Object.assign
-
-    return intersectionObserver
+    return window.IntersectionObserver && window.IntersectionObserverEntry
   }
 
   // Add polyfills via a script tag appended to the document head:
   const loadScript = url => {
     let script = document.createElement('script')
     script.src = url
-    script.onload = () => console.log(`Successfully loaded script ${url}`)
-    script.onerror = () => console.log(`Failed to load script ${url}`)
+    script.onload = 'console.log(`Successfully loaded script ${url}`)'
+    script.onerror = 'console.log(`Failed to load script ${url}`)'
     document.head.appendChild(script)
   }
 
@@ -45,7 +42,8 @@ exports.onClientEntry = () => {
   if (!browserSupportsAllJsFeatures()) {
     console.log('About to load polyfills!')
     // See: https://polyfill.io/v2/docs/examples
-    const intersectionObserver = `IntersectionObserver,IntersectionObserverEntry,Object.assign`
+    const intersectionObserver = `Object.assign,IntersectionObserver,IntersectionObserverEntry`
+
     loadScript(
       `https://cdn.polyfill.io/v2/polyfill.js?features=${intersectionObserver}&flags=gated&rum=1`
     )
