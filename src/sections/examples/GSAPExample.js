ad0a39cdf5ae14beda37a41aa8156697d1a44513
Michael Uloth <hello@michaeluloth.com>
2018-05-25 16:13:31 -0400

    Fixed logical errors in GSAPExample and improved it by converting it to a state chart approach

--- src/sections/examples/GSAPExample.js
+++ src/sections/examples/GSAPExample.js
@@ -1,48 +1,17 @@
-// DOCS: https://greensock.com/get-started-js
-// DOCS: https://greensock.com/docs/TweenMax
-// DOCS: https://greensock.com/docs/TimelineMax
-
-// TODO: see https://greensock.com/forums/topic/15749-gsap-with-create-react-app/
-// NOTE: Node with ref needs to be outside the Waypoint component
-
-// EXAMPLE: https://codepen.io/osublake/pen/0d4742d2200d028ed42297cb874af2b5?editors=0010
-
-// DOCS: https://github.com/muicss/loadjs#documentation
-
-// TODO: abstract the TweenMax.to() component above into a separate <Animate /> component? Or adapt <Reveal /> to be either .from() or .to()...?
-// const GSAPTest2 = () => {
-//   <Reveal
-//   css={{ opacity: 0, transform: `translateY(40px) scale(.8)` }}
-//     delay={index * 0.3 + 0.1}
-//     duration={1}
-//     offsetTop={-100}
-//   >
-
-//   </Reveal>
-// }
-
 class GSAPExample extends React.Component {
-  state = { revealed: false, repeat: true, animation: null }
+  // Waypoint handlers
+  handleWaypointEnter = () => this.props.transition(`ANIMATE`)
+  handleWaypointLeave = () => this.props.transition(`PAUSE`)
 
-  handleWaypointEnter = () => {
-    // Only animate if it hasn't been revealed yet (or has been reset to animate again)
-    if (!this.state.revealed) {
-      this.setState({ revealed: true })
-      this.animate()
-    }
-  }
+  // Button handlers
+  handleStartAnimation = () => this.props.transition(`ANIMATE`)
+  handlePauseAnimation = () => this.props.transition(`PAUSE`)
+  handleKillAnimation = () => this.props.transition(`KILL`)
 
-  handleWaypointLeave = () => {
-    // If animation should repeat on next reveal, reset it when leaves viewport
-    if (this.state.revealed && this.state.repeat) {
-      this.setState({ revealed: false })
-      this.killAnimation()
-    }
-  }
-
-  animate = () => {
+  // Imperative GSAP methods
+  startAnimation = () => {
     loadjs.ready(`gsap`, () => {
-      TweenMax.to(this.box, 1.5, {
+      this.box.animation = TweenMax.to(this.box, 1.5, {
         scale: 0.9,
         ease: `Power2.easeInOut`,
         repeat: -1,
@@ -51,27 +20,100 @@ class GSAPExample extends React.Component {
     })
   }
 
+  pauseAnimation = () => loadjs.ready(`gsap`, () => this.box.animation.pause())
+
+  resumeAnimation = () => loadjs.ready(`gsap`, () => this.box.animation.resume())
+
   killAnimation = () => {
-    loadjs.ready(`gsap`, () => TweenMax.killTweensOf(this.box))
+    loadjs.ready(`gsap`, () => this.box.animation.kill())
+    TweenMax.set(this.box, { clearProps: `all` })
+    this.box.animation = null
   }
 
   render() {
+    console.log(`🗺`, this.props.machineState.value)
+
     return (
-      <div ref={el => (this.box = el)}>
-        <Waypoint onEnter={this.handleWaypointEnter} onLeave={this.handleWaypointLeave}>
+      <section ref={el => (this.box = el)}>
+        <Waypoint
+          onEnter={this.handleWaypointEnter}
+          onLeave={this.handleWaypointLeave}
+        >
           <div className="mv6 bg-red pa5 shadow-lg">
             <h2>I'm an animated component</h2>
             <p className="mv4 ml-auto mr-auto measure-narrow lh-copy">
               I animate on scroll or by clicking the buttons below.
             </p>
-            <button onClick={this.animate} className="pr4">
-              Breathe!
-            </button>
-            <button onClick={this.killAnimation}>Stop breathing!</button>
+
+            <Action
+              show={[`pauseAnimation`, `killAnimation`]}
+              hide={[`startAnimation`, `resumeAnimation`]}
+            >
+              <button onClick={this.handleStartAnimation} className="ph3">
+                Breathe!
+              </button>
+            </Action>
+
+            <Action
+              show={[`startAnimation`, `resumeAnimation`]}
+              hide={[`pauseAnimation`, `killAnimation`]}
+            >
+              <button onClick={this.handlePauseAnimation} className="ph3">
+                Pause breathing!
+              </button>
+            </Action>
+
+            <Action
+              show={[`startAnimation`, `resumeAnimation`, `pauseAnimation`]}
+              hide={[`killAnimation`]}
+            >
+              <button onClick={this.handleKillAnimation} className="ph3">
+                No more breathing!
+              </button>
+            </Action>
           </div>
         </Waypoint>
-      </div>
+      </section>
     )
+  }
+}
+
+/*
+ *
+ * Animation State Chart
+ * 
+ */
+
+const animationChart = {
+  initial: `idle`,
+  states: {
+    idle: {
+      on: {
+        ANIMATE: { animated: { actions: [`startAnimation`] } }
+      }
+    },
+
+    animated: {
+      on: {
+        PAUSE: `paused`,
+        KILL: `killed`
+      }
+    },
+
+    paused: {
+      onEntry: `pauseAnimation`,
+      on: {
+        ANIMATE: { animated: { actions: [`resumeAnimation`] } },
+        KILL: `killed`
+      }
+    },
+
+    killed: {
+      onEntry: `killAnimation`,
+      on: {
+        ANIMATE: { animated: { actions: [`startAnimation`] } }
+      }
+    }
   }
 }
 
@@ -84,5 +126,6 @@ class GSAPExample extends React.Component {
 import React from 'react'
 import Waypoint from 'react-waypoint'
 import loadjs from 'loadjs'
+import { Action, withStatechart } from 'react-automata'
 
-export default GSAPExample
+export default withStatechart(animationChart)(GSAPExample)
