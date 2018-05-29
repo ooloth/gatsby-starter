e19486154e4e85deeca6f070b70ffc2f01bb593e
Michael Uloth <hello@michaeluloth.com>
2018-05-29 10:27:51 -0400

    Ignore flickity-component during build

--- gatsby-node.js
+++ gatsby-node.js
@@ -8,70 +8,71 @@ const ExtractTextPlugin = require(`extract-text-webpack-plugin`)
 
 exports.modifyWebpackConfig = ({ config, stage }) => {
   switch (stage) {
-  case `develop`:
-    // Remove default loaders
-    config.removeLoader(`css`)
-    config.removeLoader(`less`)
-    config.removeLoader(`sass`)
-    config.removeLoader(`cssModules`)
-    config.removeLoader(`lessModules`)
-    config.removeLoader(`sassModules`)
+    case `develop`:
+      // Remove default loaders
+      config.removeLoader(`css`)
+      config.removeLoader(`less`)
+      config.removeLoader(`sass`)
+      config.removeLoader(`cssModules`)
+      config.removeLoader(`lessModules`)
+      config.removeLoader(`sassModules`)
 
-    // Remove postcss from Gatsby's dev process and ignore partials
-    config.loader(`css`, {
-      test: /\.css$/,
-      exclude: [
-        /src\/styles\/components/,
-        /src\/styles\/fonts/,
-        /src\/styles\/plugins/,
-        /src\/styles\/reset/,
-        /src\/styles\/supports/,
-        /src\/styles\/utilities/
-      ],
-      loaders: [`style`, `css`]
-    })
+      // Remove postcss from Gatsby's dev process and ignore partials
+      config.loader(`css`, {
+        test: /\.css$/,
+        exclude: [
+          /src\/styles\/components/,
+          /src\/styles\/fonts/,
+          /src\/styles\/plugins/,
+          /src\/styles\/reset/,
+          /src\/styles\/supports/,
+          /src\/styles\/utilities/
+        ],
+        loaders: [`style`, `css`]
+      })
 
-    break
+      break
 
-  case `build-css`:
-    // Remove default loaders
-    config.removeLoader(`css`)
-    config.removeLoader(`less`)
-    config.removeLoader(`sass`)
-    config.removeLoader(`cssModules`)
-    config.removeLoader(`lessModules`)
-    config.removeLoader(`sassModules`)
+    case `build-css`:
+      // Remove default loaders
+      config.removeLoader(`css`)
+      config.removeLoader(`less`)
+      config.removeLoader(`sass`)
+      config.removeLoader(`cssModules`)
+      config.removeLoader(`lessModules`)
+      config.removeLoader(`sassModules`)
 
-    // Remove postcss from Gatsby's build process and ignore partials
-    config.loader(`css`, {
-      test: /\.css$/,
-      exclude: [
-        /src\/styles\/base/,
-        /src\/styles\/builds\/after-postcss/,
-        /src\/styles\/components/,
-        /src\/styles\/plugins/,
-        /src\/styles\/reset/,
-        /src\/styles\/supports/,
-        /src\/styles\/utilities/
-      ],
-      loader: ExtractTextPlugin.extract([`css?minimize`])
-    })
+      // Remove postcss from Gatsby's build process and ignore partials
+      config.loader(`css`, {
+        test: /\.css$/,
+        exclude: [
+          /src\/styles\/base/,
+          /src\/styles\/builds\/after-postcss/,
+          /src\/styles\/components/,
+          /src\/styles\/plugins/,
+          /src\/styles\/reset/,
+          /src\/styles\/supports/,
+          /src\/styles\/utilities/
+        ],
+        loader: ExtractTextPlugin.extract([`css?minimize`])
+      })
 
-    break
+      break
 
-  case `build-html`:
-    // Ignore packages that causes errors during build because they refer to the document/window (make test an array if > 1):
-    config.loader(`null`, {
-      test: [
-        /intersection-observer/,
-        /lightbox-react/,
-        /react-image-lightbox/,
-        /twitter-fetcher/
-      ],
-      loader: `null-loader`
-    })
+    case `build-html`:
+      // Ignore packages that causes errors during build because they refer to the document/window (make test an array if > 1):
+      config.loader(`null`, {
+        test: [
+          /flickity-component/,
+          /intersection-observer/,
+          /lightbox-react/,
+          /react-image-lightbox/,
+          /twitter-fetcher/
+        ],
+        loader: `null-loader`
+      })
 
-    break
+      break
   }
   return config
 }
