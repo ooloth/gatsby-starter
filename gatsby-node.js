fb8db61b17f80baa00b22ad3bc51300a8adba05f
Michael Uloth <hello@michaeluloth.com>
2018-05-29 11:10:51 -0400

    Deploy without flickity example

--- gatsby-node.js
+++ gatsby-node.js
@@ -65,7 +65,7 @@ exports.modifyWebpackConfig = ({ config, stage }) => {
       test: [
         /intersection-observer/,
         /lightbox-react/,
-        /flickity/,
+        /react-flickity-component/,
         /react-image-lightbox/,
         /twitter-fetcher/
       ],
