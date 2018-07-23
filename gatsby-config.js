1e34a36a00a7bb21e5a1ec5204462bfaa64a0e09
Michael Uloth <hello@michaeluloth.com>
2018-07-23 11:07:52 -0400

    Remove webpack-bundle-analyzer gatsby plugin (doesn't work with v2)

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,15 +36,7 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
-    `gatsby-transformer-yaml`,
-    {
-      resolve: `gatsby-plugin-webpack-bundle-analyzer`,
-      options: {
-        defaultSizes: `gzip`,
-        disable: false,
-        production: true
-      }
-    }
+    `gatsby-transformer-yaml`
     // `gatsby-plugin-sitemap`,
     // {
     //   resolve: `gatsby-plugin-robots-txt`,
