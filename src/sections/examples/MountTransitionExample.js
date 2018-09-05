62cfedfb16a3645f066263474080457d001a5ef6
Michael Uloth <hello@michaeluloth.com>
2018-09-05 17:12:03 -0400

    Replace the react-pose mount transitions example (PoseGroup replaces ReactTransitionGroup)

--- src/sections/examples/MountTransitionExample.js
+++ src/sections/examples/MountTransitionExample.js
@@ -9,8 +9,8 @@ const MountTransitionExample = () => (
     <h3 className="mb4">GSAP version</h3>
     <AnimatingBoxes />
 
-    {/* <h3 className="pt5 mb4">React Pose version</h3>
-    <AnimatingPoses /> */}
+    <h3 className="pt5 mb4">React Pose version</h3>
+    <AnimatingPoses />
   </section>
 )
 
@@ -68,62 +68,64 @@ class AnimatingBoxes extends Component {
  *
  */
 
-// class AnimatingPoses extends Component {
-//   // These contrived "boxes" stand in for real list items like images, events, etc.
-//   state = { boxes: [{ text: `box`, id: 1 }] }
+class AnimatingPoses extends Component {
+  // These contrived "boxes" stand in for real list items like images, events, etc.
+  state = { boxes: [{ text: `box`, id: 1 }] }
 
-//   addBox = () =>
-//     this.setState({
-//       boxes: this.state.boxes.concat({ text: `box`, id: shortid.generate() })
-//     })
+  addBox = () =>
+    this.setState({
+      boxes: this.state.boxes.concat({ text: `box`, id: shortid.generate() })
+    })
 
-//   removeBox = () => {
-//     this.setState({ boxes: this.state.boxes.slice(0, this.state.boxes.length - 1) })
-//   }
+  removeBox = () => {
+    this.setState({ boxes: this.state.boxes.slice(0, this.state.boxes.length - 1) })
+  }
 
-//   render() {
-//     const { boxes } = this.state
+  render() {
+    const { boxes } = this.state
 
-//     return (
-//       <>
-//         <ul>
-//           {/* the "flipMove" prop determines if the space collapses after the animation completes or before */}
-//           <PoseGroup>
-//             {boxes.map(box => (
-//               <Box
-//                 key={box.id}
-//                 className="mv2 ml-auto mr-auto w-50 mw5 b--black bw2 bg-pink pa4"
-//               >
-//                 box
-//               </Box>
-//             ))}
-//           </PoseGroup>
-//         </ul>
+    return (
+      <>
+        <ul>
+          {/* the "flipMove" prop determines if the space collapses after the animation completes or before */}
+          <PoseGroup animateOnMount={true} flipMove={false}>
+            {boxes.map(box => (
+              <Box
+                key={box.id}
+                className="mv2 ml-auto mr-auto w-50 mw5 b--black bw2 bg-pink pa4"
+              >
+                box
+              </Box>
+            ))}
+          </PoseGroup>
+        </ul>
 
-//         {/* TIP: avoid margins right next to the mount animations (causes jank) */}
-//         <div>
-//           <button onClick={this.addBox} className="btn mt4 mr2">
-//             +
-//           </button>
-//           <button onClick={this.removeBox} className="btn ml2">
-//             -
-//           </button>
-//         </div>
-//       </>
-//     )
-//   }
-// }
+        {/* TIP: avoid margins right next to the mount animations (causes jank) */}
+        <div>
+          <button onClick={this.addBox} className="btn mt4 mr2">
+            +
+          </button>
+          <button onClick={this.removeBox} className="btn ml2">
+            -
+          </button>
+        </div>
+      </>
+    )
+  }
+}
 
-// const Box = posed.li({
-//   enter: {
-//     scale: 1,
-//     transition: { type: 'spring', stiffness: 500, damping: 29 }
-//   },
-//   exit: {
-//     scale: 0,
-//     transition: { duration: 0 }
-//   }
-// })
+const List = posed.ul()
+
+const Box = posed.li({
+  enter: {
+    scale: 1
+    // transition: { type: 'spring', stiffness: 500, damping: 29 }
+  },
+  exit: {
+    scale: 0
+    // transition: { duration: 0 }
+  }
+})
 
 /* 
  *
@@ -132,7 +134,7 @@ class AnimatingBoxes extends Component {
  */
 
 import React, { Component } from 'react'
-// import posed, { PoseGroup } from 'react-pose'
+import posed, { PoseGroup } from 'react-pose'
 import TransitionGroup from 'react-transition-group/TransitionGroup'
 import shortid from 'shortid'
 // import { Transition, animated, interpolate } from 'react-spring'
