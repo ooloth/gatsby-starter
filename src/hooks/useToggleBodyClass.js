1e3d5b6f9bb8fb02f26f778268e2294aa9ac9a6d
Michael Uloth <hello@michaeluloth.com>
2018-11-19 14:32:00 -0500

    Add useToggleBodyClass hook

---
+++ src/hooks/useToggleBodyClass.js
@@ -0,0 +1,29 @@
+function useToggleBodyClass(value, [on, off]) {
+  useEffect(() => {
+    document.body.classList.remove(value ? off : on)
+    document.body.classList.add(value ? on : off)
+  })
+}
+
+/*
+ *
+ * Imports & Exports
+ *
+ */
+
+import { useEffect } from 'React'
+
+export default useToggleBodyClass
+
+/*
+ *
+ * Usage
+ *
+ */
+
+// function Home() {
+//   const [night, setNight] = useState(false)
+//   useToggleBodyClass(night, [`dark`, `light`])
+// }
+
+// See: https://twitter.com/thekitze/status/1060835281090285568
