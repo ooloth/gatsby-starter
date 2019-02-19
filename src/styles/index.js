ae7169c1cdd226d3a5d108427995a2c04b849fd9
Michael Uloth <hello@michaeluloth.com>
2019-02-19 17:22:30 -0500

    📦NEW: add common styling for SVGs used as icons

--- src/styles/index.js
+++ src/styles/index.js
@@ -3,4 +3,5 @@ export { default as Reset } from './base/Reset'
 
 export { container } from './mixins/container'
 export { copy } from './mixins/copy'
+export { icon } from './mixins/examples/icon'
 export { media } from './mixins/media'
