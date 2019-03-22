aa421066f5e47a99fab81777e460aeeed9721a6f
Michael Uloth <hello@michaeluloth.com>
2019-03-22 13:31:42 -0400

    📦add CSS enter animation from AV's site

--- src/styles/index.js
+++ src/styles/index.js
@@ -3,5 +3,6 @@ export { default as Reset } from './base/Reset'
 
 export { container } from './mixins/container'
 export { copy } from './mixins/copy'
+export { enter } from './mixins/examples/enter'
 export { icon } from './mixins/examples/icon'
 export { media } from './mixins/media'
---
+++ src/styles/mixins/examples/enter.js
@@ -0,0 +1,15 @@
+export const enter = keyframes`
+  from {
+    opacity: 0;
+    transform: scale(0.9);
+  }
+  to {
+    opacity: 1;
+    transform: scale(1);
+    visibility: visible;
+  }
+`
+
+///////////////////////////////////////////////////////////////////////////////////
+
+import { keyframes } from 'styled-components'
