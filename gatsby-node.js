1c439486f749dabc3ea9d9a5acc66d00b104c70a
Michael Uloth <hello@michaeluloth.com>
2019-03-22 12:46:23 -0400

    👌clarify page generation instructions

--- gatsby-node.js
+++ gatsby-node.js
@@ -32,7 +32,7 @@ exports.onCreateWebpackConfig = ({ actions, stage }) => {
 // See: https://www.gatsbyjs.org/docs/debugging-async-lifecycles/
 
 exports.createPages = async function({ actions, graphql }) {
-  // TODO: update `allTemplatesYaml` to correct file name
+  // TODO: update `allTemplatesYaml` to correct data file name
   await graphql(`
     {
       allTemplatesYaml {
@@ -44,11 +44,11 @@ exports.createPages = async function({ actions, graphql }) {
       }
     }
   `).then(result => {
-    // TODO: update `allTemplatesYaml` to correct file name
+    // TODO: update `allTemplatesYaml` to correct data file name
     result.data.allTemplatesYaml.edges.forEach(edge => {
       actions.createPage({
         path: edge.node.slug,
-        // TODO: update `Template.js` to correct file name
+        // TODO: update `Template.js` to correct template file name and path
         component: require.resolve(`./src/ui/@ex-templates/Template.js`),
         context: {
           slug: edge.node.slug, // this variable will be available in query
